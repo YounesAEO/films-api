226904f5ff2129d7163a763ce85b90f79e3e23bd
Ossama Zaid <ossama@screendy.com>
2021-01-09 17:16:01 +0100

    Initial commit and project bootstrap

---
+++ src/models/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import User from "./user";
+import Message from "./message";
+
+const connectDb = () => {
+  return mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+};
+
+const models = { User, Message };
+
+export { connectDb };
+
+export default models;
